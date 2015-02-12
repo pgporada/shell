7b50999f90fca91fa13a54b691b92594834759e1
Phil <philporada@gmail.com>
2015-02-12 08:31:31 -0500

    Fixes the mouse buttons on my 3M stick mouse

---
+++ mouse-wheel-emulation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xinput set-prop 'Forward USB Optical Mouse' 'Evdev Wheel Emulation' 1
+xinput set-prop 'Forward USB Optical Mouse' 'Evdev Wheel Emulation Button' 2
+xinput set-prop 'Forward USB Optical Mouse' 'Evdev Wheel Emulation Axes' 6 7 4 5
